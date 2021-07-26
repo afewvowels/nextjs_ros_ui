a9c4378a69265ab44e31648438b7fcc21ce85365
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2021-07-25 21:15:39 -0400

    updated

--- components/elements/Topics.jsx
+++ components/elements/Topics.jsx
@@ -8,9 +8,13 @@ const Topics = () => {
   const [topics, set_topics] = useState('no topics polled')
 
   useEffect(() => {
-    var rosConn = new ROSLIB.Ros({
-      url: 'ws://localhost:9090'
-    })
+    try {
+      var rosConn = new ROSLIB.Ros({
+        url: 'ws://localhost:9090'
+      })
+    } catch {
+      console.error('error connecting websockets')
+    }
 
     rosConn.on('connection', function() {set_msg('connected')})
     rosConn.on('error', function() {set_msg('error')})

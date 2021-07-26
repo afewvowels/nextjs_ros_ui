317863517dda6d926b4f35cab35c3d023675ddda
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2021-07-26 12:26:02 -0400

    push update

--- components/elements/Topics.jsx
+++ components/elements/Topics.jsx
@@ -55,7 +55,7 @@ const Topics = () => {
   return(<>
     <h2>Topics List</h2>
     <p>Status: {msg}</p>
-    <Image layout='fill' alt='ROS camera tag' src="http://localhost:8080/stream?topic=/camera/color/image_raw&amp;quality=20"/>
+    <Image layout='fill' alt='ROS camera tag' src="http://rbt-bertha-agx:8080/stream?topic=/camera/color/image_raw&amp;quality=20"/>
     <p>Topics</p>
     <ul ref={topicsRef}></ul>
     <button onClick={getTopics}>Get Topics</button>

d5eb1b49c73fb8f137c9386f0c2fe9d70acbb219
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2021-07-26 20:03:09 -0400

    push change

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 // next.config.js
 const withImages = require('next-images')
-module.exports = [
-  withImages({
-    webpack(config, options) {
-      return config
-    }
-  }),
-  { reactStrictMode: true }
-]
+module.exports = withImages({
+  webpack(config, options) {
+    return config
+  }
+})
+
+// module.exports = {
+//   reactStrictMode: true,
+// }
